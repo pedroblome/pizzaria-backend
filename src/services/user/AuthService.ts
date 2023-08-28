54f09a97db104046e4fcb7747b16b549733638ac
pedroblome <joaopedroblome78@gmail.com>
2023-08-28 09:25:39 -0300

    autentication with jwt

--- src/services/user/AuthService.ts
+++ src/services/user/AuthService.ts
@@ -1,6 +1,6 @@
 import prismaClient from "../../prisma";
-import {compare} from 'bcryptjs'
-
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 interface AuthRequest {
   email: string;
@@ -10,26 +10,36 @@ interface AuthRequest {
 class AuthUserService {
   async execute({ email, password }: AuthRequest) {
     const user = await prismaClient.user.findFirst({
-        where:{
-            email: email
-        }
-    })
-    if(!user){
-        throw new Error("user/password is incorrect")
+      where: {
+        email: email,
+      },
+    });
+    if (!user) {
+      throw new Error("user/password is incorrect");
     }
-    const passwordMatch = await compare(password, user.password)
-    if(!passwordMatch){
-        throw new Error("user/password is incorrect")
+    const passwordMatch = await compare(password, user.password);
+    if (!passwordMatch) {
+      throw new Error("user/password is incorrect");
     }
-    return {ok:true}
-
-
-
-
-
-
-    return { ok: true };
+    //all verifications done, the user can login in app
+    const token = sign(
+      {
+        name: user.name,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "10d",
+      }
+    );
+    return {
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      token: token,
+    };
   }
 }
 
-export {AuthUserService}
+export { AuthUserService };
